5abb88da1d096c9f9fe8cf96fd97705672d005ff
Ali5545 <147171399+Ali5545@users.noreply.github.com>
2025-09-26 03:08:44 -0400

    Update Studentinfos.java

--- Studentinfos.java
+++ Studentinfos.java
@@ -3,9 +3,33 @@ package Javapack;
 import java.util.Scanner;
 
 public class Studentinfos {
-
+	Scanner input = new Scanner(System.in);
+	private Studentmain student1, student2, student3;
+	private int total
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		System.out.print("How many students would you like to enter? (max 3): ");
+		int studentcount= Scanner.nextInt();
+		
+	}
+	public void mainmenu()
+	{
+		String menu = "\n\t********** Student Menu **********\n" +
+                "\t1. Promote a student\n" +
+                "\t2. Update a student's GPA\n" +
+                "\t3. View student info\n" +
+                "\t4. Exit\n"+
+                "\t5***********************************\n"+
+                "\tEnter your choice (1-4): "; 
+		System.out.print(menu);
+		
+		int choice = Scanner.nextInt();
+		
+		if (choice == 5)
+		{
+			System.out.println("Goodbye!");
+			return;
+		}
+		
 
 	}
 
