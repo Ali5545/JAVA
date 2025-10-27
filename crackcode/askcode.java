561ca4d98219b8d3b4c0db6ae51c9c1a5dfa3cfe
Ali5545 <147171399+Ali5545@users.noreply.github.com>
2025-10-27 08:01:09 -0400

    Add files via upload

---
+++ crackcode/askcode.java
@@ -0,0 +1,10 @@
+package crackcode;
+
+public class askcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ crackcode/code.java
@@ -0,0 +1,14 @@
+package crackcode;
+
+public class code {
+	private String name;
+	private String answer;
+	private int firstcode;
+	private String secondcode;
+	private int thirdcode;
+
+	public code(String num, String ans, int firstcode, String secondcode, int thirdcode)
+	{
+		this.name=num;
+	}
+}
