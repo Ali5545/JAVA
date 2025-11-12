b07ece3d4a32df898d5917dfa3d307486f6624c8
Ali5545 <147171399+Ali5545@users.noreply.github.com>
2025-11-12 06:46:18 -0500

    Refactor Collectinfo class with improved comments
    Updated comments for clarity and code structure.

--- Grade/src/Grade/Collectinfo.java
+++ Grade/src/Grade/Collectinfo.java
@@ -3,44 +3,44 @@ package Grade;
 import java.util.Scanner;
 
 public class Collectinfo {
-	static Scanner input = new Scanner(System.in);
+	static Scanner input = new Scanner(System.in); //Scanner to take inputs from the user
 	
-	public Collectinfo()
+	public Collectinfo()      //Calls askinfo 
 	{
 		askinfo();
 	}
 	
-	public void askinfo()
+	public void askinfo()            //Ask the user how many grades they want to calculate
 	{
-		float numgrades;
+		float numgrades;             //labels what type of variable they are
 		boolean yesorno;
 		
 		System.out.print("How many grades will you be averaging? ");
 		numgrades= input.nextFloat();
-		yesorno= CheckNum(numgrades);
+		yesorno= CheckNum(numgrades);    //checks out if the input is an integer 
 		if (yesorno == false)
 		{
 			System.out.print("Please enter a whole number (1-100)");
 			askinfo();
 		}
-		else if (yesorno==true) 
+		else if (yesorno==true) //if the input is correct then it gets sent to another class (calculate())
 		{
 			Calculate input = new Calculate((int)numgrades);
 		}
 	}
 	
-	public boolean CheckNum(float gradenum)
+	public boolean CheckNum(float gradenum)   
 	{
 		boolean boolstate= true;
 		
-		float isyes = gradenum % 1;
+		float isyes = gradenum % 1; //checks if it is a decimal or integer
 		if (isyes >0)
 		{
 			return false;
 		}
 		else 
 		{
-			if (gradenum <1 || gradenum >=100)
+			if (gradenum <1 || gradenum >=100)  //checks if the number is within the rang between 1-100
 			{
 				return false;
 			}
@@ -52,3 +52,4 @@ public class Collectinfo {
 	}
 
 }
+
