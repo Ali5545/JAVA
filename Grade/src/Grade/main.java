7be324fe5ba0d85561b29ea5587ef9a278f01a96
Ali5545 <147171399+Ali5545@users.noreply.github.com>
2025-11-12 06:45:50 -0500

    Clarify main method with a comment
    Added a comment to clarify the purpose of the main method.

--- Grade/src/Grade/main.java
+++ Grade/src/Grade/main.java
@@ -2,7 +2,7 @@ package Grade;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {       //Calls to Collectinfo to start the program
 		Collectinfo input = new Collectinfo();
 	}
 
