06159335b8779bca748ced94b9c5b8c910615b84
Ali5545 <147171399+Ali5545@users.noreply.github.com>
2025-12-02 01:50:37 -0500

    Add files via upload

---
+++ src/javaPack/ArrayChar.java
@@ -0,0 +1,61 @@
+package javaPack;
+
+import java.util.Scanner;
+
+public class ArrayChar extends MainProgram{
+	Scanner allinput=new Scanner(System.in);
+	
+	public void ArrayChar()
+	{
+		AcceptInfo();
+	}
+	
+	public void AcceptInfo()
+	{
+		float numletters= allinput.nextFloat(); //Set a variable for the n amount of times a letter will be printed
+		if(numletters % 1 !=0) //Checks if the number is a integer not a decimal 
+		{
+			System.out.print("incorrect value has been entered.\n");
+			MainProgram.main(null);  //goes back to the start
+		}
+		else
+		{
+			int letternum= (int) numletters; //converts float to int
+			allinput.nextLine();
+			System.out.print("Enter a word: ");
+			String wordent = allinput.nextLine();
+			CheckWord(wordent, letternum);
+		}
+	}
+	
+	private void CheckWord(String entword, int letternum)
+	{
+		int wordlen= entword.length(); //strips the word to word length to check if the user entered a word
+		if(wordlen==0)
+		{
+			MainProgram.main(null);
+		}
+		else
+		{
+			char[] wordarray= new char[wordlen];
+			for(int i=0; i<wordlen; i++)
+			{
+				wordarray[i]=entword.charAt(i);
+				if((int)wordarray[i] <= 96 || (int)wordarray[i] >= 123)
+				{
+					System.out.print(wordarray[i]+" is not a letter!\n");
+				}
+				else
+				{
+					System.out.print(wordarray[i]+ " Is a letter!\n");
+					
+					for (int x = 0; letternum>x; x++)
+					{
+						System.out.println(wordarray[i]);
+					}
+				}
+			}
+		}
+	}
+
+}
---
+++ src/javaPack/MainProgram.java
@@ -0,0 +1,15 @@
+package javaPack;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		ArrayChar arrchar= new ArrayChar();
+		System.out.println("Welcome to my letter counting words!");
+		System.out.print("This program will print out each "
+				+ "letter of a word n amount of times\n"
+				+ "Enter a number: ");
+		arrchar.AcceptInfo();
+
+	}
+
+}
